f9d5a498097013d0e6916d15117513f6836ff19c
HwangSeokBeom <tjrqja07@naver.com>
2024-11-11 16:07:14 +0900

    Initial Commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Hwangseokbeom on 11/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func numberPressed(_ sender: Any) {
+    }
+    @IBAction func operatorPressed(_ sender: Any) {
+    }
+}
+
